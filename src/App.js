f6082eb1a20389fd11213687a40c9757e63671c0
pmita <panos.mita2@outlook.com>
2021-08-10 13:40:52 +0100

    sass setup and file structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//importing our main sass file
+import './styling/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
